f0a62df1a2e706421837331a0deb893c908223c4
Christian Areas <me@areas.me>
2023-06-27 00:44:29 -0500

    Added basic error handling to parse function.

--- src/utils/parseOpenApiData.ts
+++ src/utils/parseOpenApiData.ts
@@ -2,20 +2,27 @@
 import { load } from "js-yaml"
 import { OpenApiDataType } from "../"
 
-// Parse the OpenAPI data.
+// 
 export async function parseOpenApiData(urlToOpenApiFile: string): Promise<OpenApiDataType> {
 	try {
-		// Fetch.
-		const response = await fetch(urlToOpenApiFile)
+		// Fetch the OpenAPI file.
+		const fetchResponse = await fetch(urlToOpenApiFile)
 
 		// If the response is not OK, throw an error.
-		if (!response.ok) {
-			throw new Error(response.statusText)
+		if (!fetchResponse.ok) {
+			throw new Error(`Couldn’t fetch your OpenAPI specification. Verify the file is at: ${urlToOpenApiFile}. Status: ${fetchResponse.status} ${fetchResponse.statusText}.`)
 		}
 
-		// Get the OpenAPI file’s data.
-		const openApiFile = await response.text()
-		const unvalidatedOpenApiData: any = load(openApiFile)
+		// Save the OpenAPI file.
+		const openApiFile = await fetchResponse.text()
+		
+		// Validate the OpenAPI file’s format and save the data as an object.
+		let unvalidatedOpenApiData: any
+		try {
+			unvalidatedOpenApiData = load(openApiFile)
+		} catch (error) {
+			throw new Error(`openapi-components only supports YAML files. Your file is not a YAML file. Update your OpenAPI specification and try again.`)
+		}
 
 		// Validate the OAS version.
 		const validOasVersion = "3.1.0"
@@ -29,9 +36,15 @@ export async function parseOpenApiData(urlToOpenApiFile: string): Promise<OpenAp
 		// Type the OpenAPI data.
 		const openApiData: OpenApiDataType = unvalidatedOpenApiData
 		
+		// Return the OpenAPI data.
 		return openApiData
 	} catch (error) {
-		console.error(error)
+		if (error instanceof TypeError) {
+			console.error(error)
+			throw new Error(`There’s an issue with the network. Verify the URL and try again. If the issue persists, try again later. Error: ${error.message}.`)
+		} else {
+			console.error(error)
+		}
 		throw error
 	}
 }
